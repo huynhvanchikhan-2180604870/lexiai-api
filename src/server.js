7f30b3f62bd22e5366d3008e43854d98a1521847
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2025-06-26 20:07:35 +0700

    update2

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const startServer = async () => {
     app.listen(PORT, () => {
       logger.info(`Server đang chạy trên cổng ${PORT}`);
       logger.info(`Truy cập: http://localhost:${PORT}`);
-      logger.info(`ULR CLIENT: ${process.env.FRONTEND_URL}`);
+      console.log(`ULR CLIENT: ${process.env.FRONTEND_URL}`);
     });
   } catch (error) {
     logger.error(`Lỗi khi khởi động server: ${error.message}`);
